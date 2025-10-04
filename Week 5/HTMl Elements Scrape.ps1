7ea5e431cec54c84ecf7100217027a43046bf5ce
Leochrapla <143446090+Leochrapla@users.noreply.github.com>
2025-10-04 19:59:59 -0400

    Week 5 Upload

---
+++ Week 5/HTMl Elements Scrape.ps1
@@ -0,0 +1,11 @@
+﻿$scraped_page = Invoke-WebRequest -TimeoutSec 10 http://10.0.17.9/ToBeScraped.html
+
+#get a count of links
+#$scraped_page.links | Select-Object outerText, href 
+#$h2s=$scraped_page.ParsedHtml.body.getElementsByTagName("h2") | Select-Object outerText
+#$h2s
+
+$divs1=$scraped_page.ParsedHtml.body.getElementsByTagName("div") | where {
+$_.className -eq "div-1"} | select innerText
+
+$divs1
---
+++ Week 5/ScrapeMain.ps1
@@ -0,0 +1,16 @@
+﻿. (Join-Path $PSScriptRoot ClassScrape.ps1)
+$FullTable = gatherClasses
+
+# $FullTable | Select "Class Code", Instructor, Days, "Time Start", "Time End" | where {$_."Instructor" -ilike "Furkan Paligu"}
+
+ # $FullTable | Where-Object { ($_.Location -ilike "JOYC 310") -and ($_.days -ilike "*M*") } | Sort-Object "Time Start" | Select-Object  "Time Start", "Time End", "Class Code" 
+ $ITSInstructors = $FullTable | Where-Object { ($_."Class Code" -like "SYS*") -or
+                                               ($_."Class Code" -like "NET*") -or
+                                               ($_."Class Code" -like "SEC*") -or
+                                               ($_."Class Code" -like "FOR*") -or
+                                               ($_."Class Code" -like "CSI*") -or
+                                               ($_."Class Code" -like "DAT*") } | Select-Object "Instructor" -Unique | Sort-Object "Instructor"
+
+$FullTable | Where-Object { $_.Instructor -in $ITSInstructors.Instructor } | Group-Object "Instructor" | Select-Object Count,Name | Sort-Object Count -Descending
+
+$ITSInstructors
